ee076b881837301fcc7aec55a47a865f139b5e5c
Alastair <me@atoft.dev>
2020-02-18 22:16:21 +0000

    [Build] Include build-essential in prerequisites.

--- scripts/ubuntu-prerequisites.sh
+++ scripts/ubuntu-prerequisites.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y git curl unzip tar clang cmake libglu1-mesa-dev libudev-dev libx11-dev libxrandr-dev
+apt-get install -y git curl unzip tar build-essential clang cmake libglu1-mesa-dev libudev-dev libx11-dev libxrandr-dev
