0279b82a30216b045d329d64c7994764f400ebc3
Alastair <me@atoft.dev>
2020-12-21 17:08:09 +0000

    [Build] Fix release build script.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export CXX="clang++"; 
-cd ../cmake-build-debug
+export CXX="clang++";
+mkdir ../cmake-build-release
+cd ../cmake-build-release
 cmake .. -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
 make
 ./freeplanet </dev/null &>/dev/null &
