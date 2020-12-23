245a194658b34a7e43996c43b62fbe24cc713ae2
Alastair <me@atoft.dev>
2020-12-23 18:02:13 +0000

    [Formatting] Make file headings consistent.

--- src/world/PlayerHandler.cpp
+++ src/world/PlayerHandler.cpp
@@ -1,6 +1,21 @@
-//
-// Created by alastair on 10/04/2020.
-//
+/*
+ * Copyright 2017-2020 Alastair Toft
+ *
+ * This file is part of freeplanet.
+ *
+ * freeplanet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * freeplanet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with freeplanet. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "PlayerHandler.h"
 
