c76e5f49c890afa5acf3887808a84ba2d62a6d98
Alastair <me@atoft.dev>
2021-03-21 17:25:44 +0000

    [Build] Run only on success.

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -3,5 +3,4 @@
 export CXX="clang++"; 
 cd ../cmake-build-debug
 cmake .. -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Debug
-make
-./freeplanet --forceWindowed --forceUnlockedMouse </dev/null &>/dev/null &
+make && ./freeplanet --forceWindowed --forceUnlockedMouse </dev/null &>/dev/null &
--- scripts/release.sh
+++ scripts/release.sh
@@ -4,5 +4,4 @@ export CXX="clang++";
 mkdir ../cmake-build-release
 cd ../cmake-build-release
 cmake .. -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
-make
-./freeplanet </dev/null &>/dev/null &
+make && ./freeplanet </dev/null &>/dev/null &
